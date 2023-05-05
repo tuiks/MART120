28b532f9a16f23d30b77c5db018d47e374b00e96
Taylor Denton <td200906@umconnect.umt.edu>
2023-05-05 14:01:17 -0600

    hw14

---
+++ Homework 14/sketch.js
@@ -0,0 +1,119 @@
+var characterX = 100;
+var characterY = 100;
+var w = 87;
+var s = 83;
+var a = 65;
+var d = 68;
+
+var shapeX = 30;
+var shapeY = 50;
+
+var shapeXs = [];
+var shapeYs = [];
+var diameters = [];
+
+var shapeXSpeeds = [];
+var shapeYSpeeds = [];
+
+var mouseShapeX;
+var mouseShapeY;
+
+function setup() {
+    createCanvas(500, 600);
+    for (var i = 0; i < 50; i++) {
+        shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeXs[i] = getRandomNumber(500);
+        shapeYs[i] = getRandomNumber(600);
+        diameters[i] = getRandomNumber(30);
+    }
+
+    createCharacter(200, 350);
+}
+
+function draw() {
+    background(128, 0, 0);
+    stroke(0);
+    fill(0);
+
+    createBorders(10);
+
+    textSize(32);
+    text("EXIT", width - 100, height - 50)
+
+    drawCharacter();
+    characterMovement();
+
+
+    fill(23, 40, 123);
+    for (var i = 0; i < shapeXs.length; i++) {
+        circle(shapeXs[i], shapeYs[i], diameters[i]);
+        shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeXs[i] += shapeXSpeeds[i];
+        shapeYs[i] += shapeYSpeeds[i];
+        if (shapeXs[i] > width) {
+            shapeXs[i] = 0;
+        }
+        if (shapeXs[i] < 0) {
+            shapeXs[i] = width;
+        }
+        if (shapeYs[i] > height) {
+            shapeYs[i] = 0;
+        }
+        if (shapeYs[i] < 0) {
+            shapeYs[i] = height;
+        }
+    }
+    if (characterX > width && characterY > width - 50) {
+        fill(0);
+        stroke(5);
+        textSize(26);
+        text("You Win!", width / 2 - 75, height / 2 - 75);
+    }
+
+    fill(100, 0, 0);
+    circle(mouseShapeX, mouseShapeY, 25);
+}
+
+function characterMovement() {
+    if (keyIsDown(w)) {
+        characterY -= 5;
+    }
+    if (keyIsDown(s)) {
+        characterY += 5;
+    }
+    if (keyIsDown(a)) {
+        characterX -= 5;
+        console.log("movement: " + characterX);
+    }
+    if (keyIsDown(d)) {
+        characterX += 5;
+    }
+}
+
+function createCharacter(x, y) {
+    characterX = x;
+    characterY = y;
+}
+
+function drawCharacter() {
+    fill(132, 132, 132);
+    circle(characterX, characterY, 25);
+}
+
+function createBorders(thickness) {
+    rect(0, 0, width, thickness);
+    rect(0, 0, thickness, height);
+    rect(0, height - thickness, width, thickness);
+    rect(width - thickness, 0, thickness, height - 75);
+}
+
+function mouseClicked() {
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
+
+function getRandomNumber(number) {
+    return Math.floor(Math.random() * number) + 0;
+}
